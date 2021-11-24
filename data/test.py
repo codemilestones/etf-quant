ac6fd79327988110629c9b32c81b28aaff062135
lizhe <lizhe@fenbi.com>
2021-11-24 18:01:59 +0800

    feat: add count

--- data/test.py
+++ data/test.py
@@ -7,35 +7,37 @@ from pylab import mpl
 import datetime
 import time
 
-df = pd.read_csv('sh510500.csv')
-print(df['pcnt_real_time'].value_counts())
+df = pd.read_csv('./data/sh510500.csv')
+total = len(df)
+print(total)
 
-bin = np.arange(0,20,0.5)
-print(pd.cut(df['pcnt_real_time'], bins=bin))
+bin = np.arange(0,24,0.5)
+cut = pd.cut(df['pcnt_real_time'], bins=bin, labels=bin[:-1], right=False)
+counts = pd.value_counts(cut,sort=False)
+sum_counts = total - counts.cumsum().to_frame()
 
-##绘制直方图
-fig, ax = plt.subplots()
-ax.hist(x=df['pcnt_real_time'],bins=100,
-        color="steelblue",
-        edgecolor="black",
-        cumulative=True,
-        histtype='step',
-        density=True)
-# ax.set_ylim(ax.get_ylim()[::-1])
+print(sum_counts)
 
-twin_axes=ax.twinx() 
-twin_axes.set_ylim(ax.get_ylim()[::-1])
-
-plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
-
-#添加x轴和y轴标签
-plt.xlabel("rate")
-plt.ylabel("case")
-
-#添加标题
-plt.title("分布")
-
-# plt.subplots_adjust(left=0, bottom=0, right=1, top=1)
-
-#显示图形
-plt.show()
+def jls_extract_def():
+    ##绘制直方图
+    fig, ax = plt.subplots()
+    ax.hist(x=df['pcnt_real_time'],bins=100,
+            color="steelblue",
+            edgecolor="black",
+            cumulative=True,
+            histtype='step',
+            density=True)
+    # ax.set_ylim(ax.get_ylim()[::-1])
+    
+    twin_axes=ax.twinx() 
+    twin_axes.set_ylim(ax.get_ylim()[::-1])
+    
+    plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+    
+    #添加x轴和y轴标签
+    plt.xlabel("rate")
+    plt.ylabel("case")
+    
+    #添加标题
+    plt.title("分布")
+    plt.show()
