0bc0c03aebce1f83e0e53d6276fbb598ecd8539b
lizhe <lizhe@yuanfudao.com>
2022-02-08 20:46:12 +0800

    feat: add multiple plot

--- trade_cycle/plot_etf_info.py
+++ trade_cycle/plot_etf_info.py
@@ -7,12 +7,11 @@ import math
 
 
 
-def plot_etf_info(code):
+def plot_etf_info(code, axe):
     plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
     plt.rcParams['axes.unicode_minus'] = False
-    plt.figure(figsize=(8,4))
 
-    path = "./data/" + code + ".csv"
+    path = "../data/" + code + ".csv"
     data_df = pd.read_csv(path)
     lastDayData = data_df.tail(1)
     date = lastDayData.iloc[0].at['date']
@@ -24,7 +23,7 @@ def plot_etf_info(code):
     # y=0.00010100750089083607x^3-0.0028596869977607696x^2+0.5242758923580495x+-1.2199225491198016
     recommend_position_rate = (math.pow(current_rate, 3) * 0.00008 - math.pow(current_rate, 2) * 0.00286 + current_rate * 0.524276) / 105 * 100
 
-    user_df = pd.read_csv('./trade_cycle/current_trade.csv')
+    user_df = pd.read_csv('../trade_cycle/current_trade.csv')
     select_code_data = user_df[user_df['code'] == code]
     name = select_code_data.iloc[0].at['name']
     cost_price = select_code_data.iloc[0].at['cost_price']
@@ -47,34 +46,31 @@ def plot_etf_info(code):
     if (a[3] > 100):
         a[3] = 100
     
-    plt.bar(x, b, fc='darkorange')
-    plt.bar(x, a, fc='steelblue')
+    axe.bar(x, b, fc='darkorange')
+    axe.bar(x, a, fc='steelblue')
     
     bar_num = [round(current_rate, 2), str(round(recommend_position_rate, 2))+'%', str(round(real_position_rate, 2))+'%', str(round(loss_rate, 2)) + '%']
     for xx, yy in zip(x,a):
-        plt.text(xx, yy, str(bar_num[xx]), ha='center')
+        axe.text(xx, yy, str(bar_num[xx]), ha='center')
     
     top = [max_price, '100%', '100%', '15%']
     for xx, yy in zip(x,b):
-        plt.text(xx, yy+3, str(top[xx]), ha='center')
+        axe.text(xx, yy+3, str(top[xx]), ha='center')
 
     bottom = [min_price, '0%', '0%', '0%']
     for xx, yy in zip(x,b):
-        plt.text(xx, -5, str(bottom[xx]), ha='center')
+        axe.text(xx, -5, str(bottom[xx]), ha='center')
     
     label = ['价格', '建议仓位', '实际仓位', '建仓亏损']
     for xx, yy in zip(x,b):
-        plt.text(xx, -10, label[xx], ha='center')
+        axe.text(xx, -10, label[xx], ha='center')
 
     for xx, yy in zip(x,b):
-        plt.text(xx, -15, '', ha='center')
+        axe.text(xx, -25, '', ha='center')
     
-    plt.legend()
-    plt.axis('off')
+    # plt.legend()
+    axe.axis('off')
 
-    plt.title(date + ' ' + code + ' ' + name, y=1.02)
-    plt.show()
-
-
-plot_etf_info('sh513050')
+    axe.set_title(date + ' ' + code + ' ' + name, y=1.02)
+    # plt.show()
 
